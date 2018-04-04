971dca9fac8134607c4eb83276261f33e888921c
Joel Stoker <joel.d.stoker@gmail.com>
2018-04-04 16:10:47 -0600

    added using of the pq

--- TSPSolver.py
+++ TSPSolver.py
@@ -16,6 +16,40 @@ import numpy as np
 from TSPClasses import *
 import heapq
 
+def setColumn(cost_matrix, column, value):
+    for i in range(len(cost_matrix)):
+        cost_matrix[i][column] = value
+
+def setRow(cost_matrix, row, value):
+    for i in range(len(cost_matrix)):
+        cost_matrix[row][i] = value
+
+def reduceRow(cost_matrix, row_number):
+    m = min(cost_matrix[row_number])
+    if m == float('inf'):
+        return 0
+    for i in range(len(cost_matrix[row_number])):
+        cost_matrix[row_number][i] -= m
+    return m
+
+def reduceColumn(cost_matrix, column_number):
+    column = []
+    for i in range(len(cost_matrix)):
+        column.append(cost_matrix[i][column_number])
+    m = min(column)
+    if m == float('inf'):
+        return 0
+    for i in range(len(cost_matrix)):
+        cost_matrix[i][column_number] -= m
+    return m
+
+def reduceCostMatrix(cost_matrix):
+    low_bound = 0
+    for i in range(len(cost_matrix)):
+        low_bound += reduceRow(cost_matrix, i)
+    for i in range(len(cost_matrix)):
+        low_bound += reduceColumn(cost_matrix, i)
+    return low_bound
 
 
 class TSPSolver:
@@ -86,52 +120,49 @@ not counting initial BSSF estimate)</returns> '''
     def greedy( self, start_time, time_allowance=60.0 ):
         pass
 
-    def setColumn(cost_matrix, column, value):
-        for i in range(len(cost_matrix)):
-            cost_matrix[i][column] = value
-
-    def setRow(cost_matrix, row, value):
-        for i in range(len(cost_matrix)):
-            cost_matrix[row][i] = value
-
-    def reduceRow(cost_matrix, row_number):
-        m = min(cost_matrix[row_number])
-        if m == float('inf'):
-            return 0
-        for i in range(len(cost_matrix[row_number])):
-            cost_matrix[row_number][i] -= m
-        return m
-
-    def reduceColumn(cost_matrix, column_number):
-        column = []
-        for i in range(len(cost_matrix)):
-            column.append(cost_matrix[i][column_number])
-        m = min(column)
-        if m == float('inf'):
-            return 0
-        for i in range(len(cost_matrix)):
-            cost_matrix[i][column_number] -= m
-        return m
-
-    def reduceCostMatrix(cost_matrix):
-        low_bound = 0
-        for i in range(len(cost_matrix)):
-            low_bound += reduceRow(cost_matrix, i)
-        for i in range(len(cost_matrix)):
-            low_bound += reduceColumn(cost_matrix, i)
-        return low_bound
 
     def branchAndBound( self, start_time, time_allowance=60.0 ):
         cities = self._scenario.getCities()
-        initial_cost_matrix = np.arange(len(cities)**2).reshape(len(cities), len(cities))
+        initial_cost_matrix = np.arange(float(len(cities))**2).reshape(len(cities), len(cities))
         for i in range(len(cities)):
             for j in range(len(cities)):
-                if i == j:
-                    initial_cost_matrix[i][j] = float('inf')
-                    continue
                 initial_cost_matrix[i][j] = cities[i].costTo(cities[j])
 
         low_bound = reduceCostMatrix(initial_cost_matrix)
+        
+        pq = []
+        heapq.heappush(pq, (len(cities) - 1, low_bound, [0], initial_cost_matrix))
+        pelim_results = self.defaultRandomTour(time.time())
+        bssf = {}
+        bssf['cost'] = pelim_results['cost']
+        bssf['soln'] = pelim_results['soln']
+
+        while len(pq) != 0 and time.time() - start_time < 60:
+            state = heapq.heappop(pq)
+            depth = len(cities) - state[0]
+            low_bound = state[1]
+            visited = state[2]
+            cost_matrix = state[3]
+            if depth == len(cities) - 1: # this means that the next node is the last node
+                if low_bound < bssf['cost']: # see if this is a better path
+                    bssf['cost'] = low_bound
+                    bssf['soln'] = visited
+                continue
+
+            for i in range(1, len(cities)):
+                nlow_bound = low_bound
+                if cost_matrix[visited[len(visited) - 1]][i] != float('inf'):
+                    new_cm = np.array(cost_matrix)
+                    nlow_bound += new_cm[visited[len(visited) - 1]][i]
+                    setRow(new_cm, visited[len(visited) - 1], float('inf'))
+                    setColumn(new_cm, i, float('inf'))
+                    new_cm[i][visited[len(visited) - 1]] = float('inf')
+                    nlow_bound += reduceCostMatrix(new_cm)
+                    if nlow_bound < bssf['cost']:
+                        nvisited = list(visited)
+                        heapq.heappush(pq, (len(cities) - depth - 1, nlow_bound, nvisited.append(i), new_cm))
+                    # if not better it was just pruned
+                # we can't go to this (i) verticy. It is us or already visited
 
     def fancy( self, start_time, time_allowance=60.0 ):
         pass
